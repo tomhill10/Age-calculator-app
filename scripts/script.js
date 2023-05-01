39b8f3b30486487e1f4f4a9a0ada598ea269a9ed
tomhill10 <tom@codehill.co.uk>
2023-05-01 22:22:41 +0100

    managed to get error message to work when data is invalid

--- scripts/script.js
+++ scripts/script.js
@@ -1,65 +1,44 @@
-let button = document.getElementById("button-click");
+/*let button = document.getElementById("button-click");
 
 button.addEventListener("click", function () {
   console.log("button clicked!");
 });
-
-/* CODE THAT DOESNT WORK MAYBE COME BACK TO
-let dday = document.getElementById("day");
-let mmonth = document.getElementById("month");
-let yyear = document.getElementById("year");
-
-function calculateAge() {
-let dayborn = document.getElementById("day").value;
-console.log(dayborn);
-}
+*/ //To Test eventListener
 
 function validateDate() {
-  // Split the date into day, month, and year values
-  let day = document.getElementById("day");
-  let month = document.getElementById("month");
-  let year = document.getElementById("year");
+  let day = document.getElementById("day").value;
+  let month = document.getElementById("month").value;
+  let year = document.getElementById("year").value;
 
-  // Check if the month is valid
   if (month < 1 || month > 12) {
     return false;
   }
 
-  // Check if the day is valid
   var daysInMonth = new Date(year, month, 0).getDate();
   if (day < 1 || day > daysInMonth) {
     return false;
   }
 
-  // Check if the year is valid
-  if (year < 1000 || year > 9999) {
+  if (year < 1000 || year > 2024) {
     return false;
   }
 
-  // If all checks pass, the date is valid
   return true;
 }
-console.log(validateDate());
-
-let inputBorder = document.getElementsByTagName("input");
-
-if (validateDate() === false) {
-  for (let i = 0; i < inputBorder.length; i++) {
-    inputBorder[i].style.border = "1px solid red";
-    inputBorder[i].style.fontSize = "50px";
-  }
-}
-
-let button = document.getElementsByTagName("button");
 
 button.addEventListener("click", function () {
   if (validateDate() === false) {
+    let inputBorder = document.getElementsByTagName("input");
+    let inputWarning = document.getElementsByClassName("day-error");
+    let inputLabel = document.getElementsByTagName("label");
     for (let i = 0; i < inputBorder.length; i++) {
       inputBorder[i].style.border = "1px solid red";
-      inputBorder[i].style.fontSize = "50px";
+    }
+    for (let i = 0; i < inputWarning.length; i++) {
+      inputWarning[i].style.color = "red";
+    }
+    for (let i = 0; i < inputLabel.length; i++) {
+      inputLabel[i].style.color = "red";
     }
   }
 });
-
-
-*/
